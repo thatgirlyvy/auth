50e4fbf15d39b6d885ddb59bc6f40ed1ee0a7881
Aynzt <wtchana@clicdev-consulting.com>
2022-06-30 17:48:46 +0100

    nothing

--- src/main/java/com/example/auth/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/auth/configuration/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsChecker;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
